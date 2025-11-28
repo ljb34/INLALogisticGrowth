c728d4c9647630423c042a52e042df4604770af0
ljb34 <2966362B@student.gla.ac.uk>
2025-11-28 08:55:14 +0000

    Try log/exp transforming in update rule

--- R/iterate.cgeneric.lgcp.fit.R
+++ R/iterate.cgeneric.lgcp.fit.R
@@ -71,7 +71,7 @@ iterate.cgeneric.fit.lgcp<- function(data, smesh, tmesh, samplers,prior.mean,
   print(paste("dim P = ", dim(P)))
   print(paste("dim b = ", dim(b)))
   print(class(b))
-  new.linpoint <- (1-gamma)*initial.linpoint +gamma*Matrix::solve(P,b)
+  new.linpoint <-log( (1-gamma)*exp(initial.linpoint) +gamma*exp(Matrix::solve(P,b)))
   
   #New update rule
   #weighted.means <- Map(function(v,p) v*p, mean_list, nodes$weight.prob)
@@ -141,7 +141,7 @@ iterate.cgeneric.fit.lgcp<- function(data, smesh, tmesh, samplers,prior.mean,
     P <- Reduce("+", Map(function(m, w) m * w, mat_list, nodes$weight.prob))
     weighted.means <- Map(function(v,p) v*p, mean_list, nodes$weight.prob)
     b <- Reduce("+", Map(function(m,w) m%*%w, mat_list,weighted.means))
-    new.linpoint <- (1-gamma)*lp.mat[,n] +gamma*Matrix::solve(P,b)
+    new.linpoint <- log((1-gamma)*exp(lp.mat[,n]) +gamma*exp(Matrix::solve(P,b)))
     
     #New update rule
     #weighted.means <- Map(function(v,p) v*p, mean_list, nodes$weight.prob)
